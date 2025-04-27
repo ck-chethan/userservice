e47b1468226ea903c5f874abf6cea069604f2729
Chethan C K <chethanckm66@gmail.com>
2025-04-27 19:49:43 +0530

    Adding JPA oauth-2 files

--- src/main/java/org/lld/userservice/configs/SecurityConfiguration.java
+++ src/main/java/org/lld/userservice/configs/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
--- src/test/java/org/lld/userservice/UserserviceApplicationTests.java
+++ src/test/java/org/lld/userservice/UserserviceApplicationTests.java
@@ -1,13 +1,49 @@
 package org.lld.userservice;
 
 import org.junit.jupiter.api.Test;
+import org.lld.userservice.security.repositories.JpaRegisteredClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.test.annotation.Commit;
+
+import java.util.UUID;
 
 @SpringBootTest
 class UserserviceApplicationTests {
+    @Autowired
+    private JpaRegisteredClientRepository jpaRegisteredClientRepository;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    @Commit
+    void storeRegisterClientIntoDatabase() {
+        // This test is to check if the storeRegisterClientIntoDatabase method works as expected
+        // You can implement the logic to test the method here
+        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("oidc-client")
+                .clientSecret("{noop}secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+                .postLogoutRedirectUri("http://127.0.0.1:8080/")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope("ADMIN")
+                .scope("USER")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+
+        jpaRegisteredClientRepository.save(oidcClient);
+
     }
 
 }
