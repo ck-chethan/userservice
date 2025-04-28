ff6df2c939c8f369ec8668384a5e2bc62da0da3c
Chethan C K <chethanckm66@gmail.com>
2025-04-28 09:38:43 +0530

    Adding changes to constructor to match the framework

--- src/main/java/org/lld/userservice/security/models/CustomGrantedAuthority.java
+++ src/main/java/org/lld/userservice/security/models/CustomGrantedAuthority.java
@@ -6,17 +6,18 @@ import org.springframework.security.core.GrantedAuthority;
 
 @JsonDeserialize
 public class CustomGrantedAuthority implements GrantedAuthority {
-    private Role role;
+//    private Role role;
+    private String authority;
 
     public CustomGrantedAuthority() {
     }
 
     public CustomGrantedAuthority(Role role) {
-        this.role = role;
+        this.authority = role.getName();
     }
 
     @Override
     public String getAuthority() {
-        return role.getName();
+        return authority;
     }
 }
--- src/main/java/org/lld/userservice/security/models/CustomUserDetails.java
+++ src/main/java/org/lld/userservice/security/models/CustomUserDetails.java
@@ -13,32 +13,71 @@ import java.util.List;
 @JsonDeserialize
 public class CustomUserDetails implements UserDetails {
 
-    private User user;
+//    private User user;
+    private String username;
+    private String password;
+    private List<CustomGrantedAuthority> authorities;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
 
     public CustomUserDetails() {}
 
     public CustomUserDetails(User user) {
-        this.user = user;
+        this.username = user.getEmail();
+        this.password = user.getHashedPassword();
+        this.authorities = new ArrayList<>();
+        if (user.getRoles() != null) {
+            for (Role role : user.getRoles()) {
+                this.authorities.add(new CustomGrantedAuthority(role));
+            }
+        }
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialsNonExpired = true;
+        this.enabled = true;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<CustomGrantedAuthority> grantedAuthorities = new ArrayList<>();
-        if (user.getRoles() != null) {
-            for (Role role : user.getRoles()) {
-                grantedAuthorities.add(new CustomGrantedAuthority(role));
-            }
-        }
-        return grantedAuthorities;
+//        List<CustomGrantedAuthority> grantedAuthorities = new ArrayList<>();
+//        if (user.getRoles() != null) {
+//            for (Role role : user.getRoles()) {
+//                grantedAuthorities.add(new CustomGrantedAuthority(role));
+//            }
+//        }
+//        return grantedAuthorities;
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return user.getHashedPassword();
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return user.getEmail();
+        return username;
     }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
 }
